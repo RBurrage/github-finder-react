6fef8231e9629cc89478f0b49a28ea14d45d8926
Robin Burrage <robin.o.burrage@gmail.com>
2022-06-15 10:49:50 -0400

    Add GithubContext and AlertContext

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -31,12 +31,16 @@ export const GithubProvider = ({ children }) => {
         })
     };
 
+    //Clear users from state
+    const clearUsers = () => dispatch({ type: 'CLEAR_USERS' });
+
     const setLoading = () => dispatch({ type: 'SET_LOADING' });
 
     return <GithubContext.Provider value={{
         users: state.users,
         loading: state.loading,
-        searchUsers
+        searchUsers,
+        clearUsers
     }}>
         {children}
     </GithubContext.Provider>
